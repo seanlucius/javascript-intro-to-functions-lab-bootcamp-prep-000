188299aefaee6f4c06db26dcd29fe98f1234cd12
Sean Lucius <seanlucius22@icloud.com>
2018-07-28 02:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
 } else if (string === string.toUpperCase) {
     return "YES INDEED!"
-} else if (string === )
+} else if (string === "I love you, Grandma")
 }
